d6587c6c697b35956fb7ed5bbd8c569caad12e7d
Michael Ledin <m.ledin@appheads.ru>
2018-11-22 16:50:43 +0300

    feat(*): chain `merge` functions, updated flavors to 4.0.0.

--- getOptions.js
+++ getOptions.js
@@ -1,10 +1,17 @@
 'use strict';
 
+const deepmerge = require('deepmerge');
+
 module.exports = (options, pluginOptions) => {
   pluginOptions = pluginOptions || {};
+  const result = deepmerge(pluginOptions, options);
+
   let loaders = options.loaders;
   if (!options.overrideLoaders && pluginOptions.loaders) {
     loaders = [...pluginOptions.loaders, ...(loaders || [])];
+  }
+  if (loaders) {
+    result.loaders = loaders;
   }
 
   let transform = options.transform;
@@ -16,12 +23,21 @@ module.exports = (options, pluginOptions) => {
       transform = pluginOptions.transform;
     }
   }
-  const result = Object.assign({}, pluginOptions, options);
-  if (loaders) {
-    result.loaders = loaders;
-  }
   if (transform) {
     result.transform = transform;
   }
+
+  let merge = options.merge;
+  if (pluginOptions.merge) {
+    if (!merge) {
+      merge = require('flavors').defaultOptions.merge;
+    }
+    const merge2 = merge, merge1 = pluginOptions.merge;
+    merge = (x, y) => deepmerge(merge2(x, y), merge1(x, y), {arrayMerge: (d, s) => s});
+  }
+  if (merge) {
+    result.merge = merge;
+  }
+
   return result;
 };
--- test/index.js
+++ test/index.js
@@ -6,18 +6,40 @@ const
 
 describe('loader', () => {
   it('overrides plugin options', () => {
-    assert.deepEqual(getOptions({ configDirName: 'config' }, { configDirName: 'pluginConfig' }), { configDirName: 'config' });
+    assert.deepEqual(getOptions({configDirName: 'config'}, {configDirName: 'pluginConfig'}), {configDirName: 'config'});
   });
   it('chains transforms', () => {
-    assert.deepEqual(getOptions({ transform: _ => ({ value3: _.value2 + 1 }) }, { transform: _ => ({ value2: _.value1 + 1 }) }).transform({ value1: 1 }), { value3: 3 });
+    assert.deepEqual(getOptions({transform: _ => ({value3: _.value2 + 1})}, {transform: _ => ({value2: _.value1 + 1})}).transform({value1: 1}), {value3: 3});
   });
   it('overrides plugin transform', () => {
-    assert.deepEqual(getOptions({ overrideTransform: true, transform: _ => ({ value3: _.value2 + 1 }) }, { transform: _ => ({ value2: _.value1 + 1 }) }).transform({ value1: 1, value2: 1 }), { value3: 2 });
+    assert.deepEqual(getOptions({
+      overrideTransform: true,
+      transform: _ => ({value3: _.value2 + 1})
+    }, {transform: _ => ({value2: _.value1 + 1})}).transform({value1: 1, value2: 1}), {value3: 2});
   });
   it('prepends plugin transformers', () => {
-    assert.deepEqual(getOptions({ loaders: [() => 1]}, { loaders: [() => 2]}).loaders.map(_ => _()), [2, 1]);
+    assert.deepEqual(getOptions({loaders: [() => 1]}, {loaders: [() => 2]}).loaders.map(_ => _()), [2, 1]);
   });
   it('overrides plugin loaders', () => {
-    assert.deepEqual(getOptions({ loaders: [() => 1], overrideLoaders: true}, { loaders: [() => 2]}).loaders.map(_ => _()), [1]);
+    assert.deepEqual(getOptions({
+      loaders: [() => 1],
+      overrideLoaders: true
+    }, {loaders: [() => 2]}).loaders.map(_ => _()), [1]);
+  });
+  it('chains merges', () => {
+    assert.deepEqual(getOptions({merge: (x, y) => ({a: x, b: y, c: 3, d: 4})}, {
+      merge: (x, y) => ({
+        c: x,
+        d: y
+      })
+    }).merge(1, 2), {a: 1, b: 2, c: 1, d: 2});
+  });
+  it('uses default merge', () => {
+    assert.deepEqual(getOptions({}, {
+      merge: (x, y) => ({
+        d: x,
+        e: y
+      })
+    }).merge({a: 1, b: 2}, {a: 3, c: 4}), {a: 3, b: 2, c: 4, d: {a: 1, b: 2}, e: {a: 3, c: 4}});
   });
 });
